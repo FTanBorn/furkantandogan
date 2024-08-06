366a2fd3b19fae6cdff196d59ceccbdecd111d67
Furkan Tandoğan <furkantandogan81@gmail.com>
2024-08-07 01:54:58 +0300

    Change Background and added Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,46 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Card, Stack, Typography } from "@mui/material";
 
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Box
-        display={"flex"}
+    <div className="homePage">
+      <Stack
+        height={"90vh"}
         justifyContent={"center"}
         alignItems={"center"}
-        padding={25}
+        display={"flex"}
       >
-        <Stack
-          direction={"column"}
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
+        <Card
+          sx={{
+            backgroundColor: "#ae9991",
+            height: "40vh",
+            width: "40vw",
+            border: "3px solid #392822 2",
+            borderRadius: "4",
+            padding: 4,
+            position: "relative",
+          }}
         >
-          <Typography textAlign={"center"} variant="h2" fontWeight={"bold"}>
-            AHSENİİ ÇOKK SEVİYORUMM
-          </Typography>
-          <FavoriteIcon sx={{ fontSize: 90, color: "red" }} />
-        </Stack>
-      </Box>
-    </main>
+          <Box
+            sx={{
+              backgroundColor: "#fffff",
+              height: "20px",
+              width: "20px",
+              border: "3px solid #392822",
+              borderRadius: "4",
+              padding: 4,
+              position: "absolute",
+              top:-5,
+              left: '10%',
+            }}
+          >
+          </Box>
+          
+        </Card>
+      </Stack>
+    </div>
   );
 }
